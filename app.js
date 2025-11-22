5467d7d52ce8ba3f2df9ed37c1b1e75be6813083
Brit.Greg <brittanniagregorygmail.com>
2025-11-21 21:16:01 -0500

    Complete AJAX implementation

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const loadButton = document.getElementById('load-data-btn');
-    const resultArea = document.getElementById('result-area');
+    const searchButton = document.getElementById('search-btn');
+    const searchField = document.getElementById('search-field');
+    const resultDiv = document.getElementById('result'); 
 
-    loadButton.addEventListener('click', function() {
-        resultArea.innerHTML = "Loading data...";
+    searchButton.addEventListener('click', function() {
+        let searchQuery = searchField.value.trim(); 
+        const encodedQuery = encodeURIComponent(searchQuery);
 
-        fetch('superheroes.php')
+        let url = 'superheroes.php';
+        if (encodedQuery) {
+            url += `?query=${encodedQuery}`;
+        }
+
+        resultDiv.innerHTML = "Loading data...";
+
+        fetch(url)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok: ' + response.statusText);
@@ -13,37 +22,11 @@ document.addEventListener('DOMContentLoaded', function() {
                 return response.text();
             })
             .then(data => {
-                resultArea.innerHTML = data;
+                resultDiv.innerHTML = data;
             })
             .catch(error => {
-                resultArea.innerHTML = 'Error fetching data: ' + error.message;
+                resultDiv.innerHTML = `<span class="not-found">Error fetching data: ${error.message}</span>`;
                 console.error('Fetch error:', error);
             });
-    });
-});
-
-$(document).ready(function() {
-    $('#load-data-btn').on('click', function() {
-        const $resultArea = $('#result-area');
-        
-        // Show a loading message
-        $resultArea.text("Loading data via jQuery...");
-
-        $.ajax({
-            url: 'superheroes.php',
-            
-            method: 'GET',
-            
-            dataType: 'html', 
-            
-            success: function(data) {
-                $resultArea.html(data);
-            },
-            
-            error: function(jqXHR, textStatus, errorThrown) {
-                $resultArea.html('Error fetching data: ' + textStatus + ' - ' + errorThrown);
-                console.error("AJAX Error:", textStatus, errorThrown);
-            }
-        });
     });
 });
--- superheroes.php
+++ superheroes.php
@@ -2,71 +2,103 @@
 
 $superheroes = [
   [
-      "id" => 1,
-      "name" => "Steve Rogers",
-      "alias" => "Captain America",
-      "biography" => "Recipient of the Super-Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world’s mightiest heroes and the leader of the Avengers.",
+    "id" => 1,
+    "name" => "Steve Rogers",
+    "alias" => "Captain America",
+    "biography" => "Recipient of the Super-Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world’s mightiest heroes and the leader of the Avengers.",
   ],
   [
-      "id" => 2,
-      "name" => "Tony Stark",
-      "alias" => "Ironman",
-      "biography" => "Genius. Billionaire. Playboy. Philanthropist. Tony Stark's confidence is only matched by his high-flying abilities as the hero called Iron Man.",
+    "id" => 2,
+    "name" => "Tony Stark",
+    "alias" => "Ironman",
+    "biography" => "Genius. Billionaire. Playboy. Philanthropist. Tony Stark's confidence is only matched by his high-flying abilities as the hero called Iron Man.",
   ],
   [
-      "id" => 3,
-      "name" => "Peter Parker",
-      "alias" => "Spiderman",
-      "biography" => "Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.",
+    "id" => 3,
+    "name" => "Peter Parker",
+    "alias" => "Spiderman",
+    "biography" => "Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.",
   ],
   [
-      "id" => 4,
-      "name" => "Carol Danvers",
-      "alias" => "Captain Marvel",
-      "biography" => "Carol Danvers becomes one of the universe's most powerful heroes when Earth is caught in the middle of a galactic war between two alien races.",
+    "id" => 4,
+    "name" => "Carol Danvers",
+    "alias" => "Captain Marvel",
+    "biography" => "Carol Danvers becomes one of the universe's most powerful heroes when Earth is caught in the middle of a galactic war between two alien races.",
   ],
   [
-      "id" => 5,
-      "name" => "Natasha Romanov",
-      "alias" => "Black Widow",
-      "biography" => "Despite super spy Natasha Romanoff’s checkered past, she’s become one of S.H.I.E.L.D.’s most deadly assassins and a frequent member of the Avengers.",
+    "id" => 5,
+    "name" => "Natasha Romanov",
+    "alias" => "Black Widow",
+    "biography" => "Despite super spy Natasha Romanoff’s checkered past, she’s become one of S.H.I.E.L.D.’s most deadly assassins and a frequent member of the Avengers.",
   ],
   [
-      "id" => 6,
-      "name" => "Bruce Banner",
-      "alias" => "Hulk",
-      "biography" => "Dr. Bruce Banner lives a life caught between the soft-spoken scientist he’s always been and the uncontrollable green monster powered by his rage.",
+    "id" => 6,
+    "name" => "Bruce Banner",
+    "alias" => "Hulk",
+    "biography" => "Dr. Bruce Banner lives a life caught between the soft-spoken scientist he’s always been and the uncontrollable green monster powered by his rage.",
   ],
   [
-      "id" => 7,
-      "name" => "Clint Barton",
-      "alias" => "Hawkeye",
-      "biography" => "A master marksman and longtime friend of Black Widow, Clint Barton serves as the Avengers’ amazing archer.",
+    "id" => 7,
+    "name" => "Clint Barton",
+    "alias" => "Hawkeye",
+    "biography" => "A master marksman and longtime friend of Black Widow, Clint Barton serves as the Avengers’ amazing archer.",
   ],
   [
-      "id" => 8,
-      "name" => "T'challa",
-      "alias" => "Black Panther",
-      "biography" => "T’Challa is the king of the secretive and highly advanced African nation of Wakanda - as well as the powerful warrior known as the Black Panther.",
+    "id" => 8,
+    "name" => "T'challa",
+    "alias" => "Black Panther",
+    "biography" => "T’Challa is the king of the secretive and highly advanced African nation of Wakanda - as well as the powerful warrior known as the Black Panther.",
   ],
   [
-      "id" => 9,
-      "name" => "Thor Odinson",
-      "alias" => "Thor",
-      "biography" => "The son of Odin uses his mighty abilities as the God of Thunder to protect his home Asgard and planet Earth alike.",
+    "id" => 9,
+    "name" => "Thor Odinson",
+    "alias" => "Thor",
+    "biography" => "The son of Odin uses his mighty abilities as the God of Thunder to protect his home Asgard and planet Earth alike.",
   ],
   [
-      "id" => 10,
-      "name" => "Wanda Maximoff",
-      "alias" => "Scarlett Witch",
-      "biography" => "Notably powerful, Wanda Maximoff has fought both against and with the Avengers, attempting to hone her abilities and do what she believes is right to help the world.",
+    "id" => 10,
+    "name" => "Wanda Maximoff",
+    "alias" => "Scarlett Witch",
+    "biography" => "Notably powerful, Wanda Maximoff has fought both against and with the Avengers, attempting to hone her abilities and do what she believes is right to help the world.",
   ], 
 ];
 
-?>
 
-<ul>
-<?php foreach ($superheroes as $superhero): ?>
-  <li><?= $superhero['alias']; ?></li>
-<?php endforeach; ?>
-</ul>
+$query = isset($_GET['query']) ? $_GET['query'] : '';
+$search_term = strtolower(trim(filter_var($query, FILTER_SANITIZE_FULL_SPECIAL_CHARS)));
+
+$found_superhero = null;
+
+if (!empty($search_term)) {
+    foreach ($superheroes as $hero) {
+        $name_match = strtolower($hero['name']) === $search_term;
+        $alias_match = strtolower($hero['alias']) === $search_term;
+        
+        if ($name_match || $alias_match) {
+            $found_superhero = $hero;
+            break; 
+        }
+    }
+
+    if ($found_superhero) {
+        ?>
+        <h2><?= strtoupper($found_superhero['alias']); ?></h2>
+        <h3>A.K.A. <?= $found_superhero['name']; ?></h3>
+        <p><?= $found_superhero['biography']; ?></p>
+        <?php
+    } else {
+        ?>
+        <span class='not-found'>SUPERHERO NOT FOUND</span>
+        <?php
+    }
+
+} else {
+    ?>
+    <ul>
+    <?php foreach ($superheroes as $superhero): ?>
+        <li><?= $superhero['alias']; ?></li>
+    <?php endforeach; ?>
+    </ul>
+    <?php
+}
+?>
